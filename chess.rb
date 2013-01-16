280719c5e901707f0202e72689fd91388a72fee0
Adam and Erik <adam.gilg@gmail.com>
2013-01-16 11:35:51 -0800

    Added in unicode values for piece names.

--- chess.rb
+++ chess.rb
@@ -2,6 +2,21 @@ require 'chess_pieces'
 
 class Chess
 
+  UNICODE_CHARS = {
+    "whiteKing" => "\u2654",
+    "whiteQueen" => "\u2655",
+    "whiteRook" => "\u2656",
+    "whiteBishop" => "\u2657",
+    "whiteKnight" => "\u2658",
+    "whitePawn" => "\u2659",
+    "blackKing" => "\u265A",
+    "blackQueen" => "\u265B",
+    "blackRook" => "\u265C",
+    "blackBishop" => "\u265D",
+    "blackKnight" => "\u265E",
+    "blackPawn" => "\u265F"
+  }
+
   def initialize
     @board = []
     build_board
@@ -20,6 +35,10 @@ class Chess
   def populate_board
     # create and put pieces in appropriate positions
     #pieces for black & white
+  end
+
+  def print_board
+
   end
 
 end
--- chess_pieces.rb
+++ chess_pieces.rb
@@ -2,15 +2,21 @@
 # 1/16/13
 
 class Piece
-
+#ask ned about way to change attrs to attr?
   attr_accessor :x_coord, :y_coord, :dead, :moved
   attr_reader :color
 
-  def initialize(x_coord, y_coord, board, color)
-    @x_coord, @y_coord, @board, @color = x_coord, y_coord, board, color
+  alias_method :dead?, :dead
+  alias_method :moved?, :moved
+
+  def initialize(color)
+    @color = color
     @dead = false
     @moved = false
+  end
 
+  def name
+    @color + self.class.to_s
   end
 
 end
