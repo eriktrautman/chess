766a4e074dd84c7c1d0fb3de95373e0b3a63d239
Adam and Erik <adam.gilg@gmail.com>
2013-01-16 14:56:36 -0800

    Abstracted the piece movement patterns.

--- chess_pieces.rb
+++ chess_pieces.rb
@@ -4,6 +4,10 @@
 class Piece
 #ask ned about way to change attrs to attr?
   BOARD_LENGTH = 8
+  DIAGONALS = [[-1,-1],[-1,1],[1,1],[1,-1]]
+  LEFT_RIGHT = [[1,0],[-1,0]]
+  FWD_BACK = [[0,1],[0-1]]
+  KNIGHT = [[-2,1],[-2,-1],[-1,2],[-1,-2],[1,2],[1,-2],[2,1],[2,-1]]
 
   attr_accessor :moved
   attr_reader :color
@@ -19,17 +23,18 @@ class Piece
     @color + self.class.to_s
   end
 
-  def in_bounds?(coordinate)
-    coordinate >= 0 && coordinate < BOARD_LENGTH
+  def in_bounds?(coordinates)
+    (0..BOARD_LENGTH-1).include?(coordinates[0] && coordinates[1])
   end
 
-  def theoretical_moves(start_row, start_column, max)
+  # outputs an array containing subarrays of theoretical moves (in order outwards)
+  def theoretical_moves(start_row, start_column)
     theoretical_moves = []
     row, column = start_row, start_column
 
     self.moves.each do |move_coord|
       move_chain = []
-      while in_bounds?(row + move_coord[0]) && in_bounds?(column + move_coord[1])
+      while in_bounds?([row + move_coord[0],column + move_coord[1]]) && move_chain.size < self.max_distance
         move_chain << [row + move_coord[0], column + move_coord[1]]
       end
       theoretical_moves << move_chain
@@ -38,14 +43,26 @@ class Piece
 
 end
 
-
+# ------------------------------------------------------------------------------------
 
 class King < Piece
-   = [[-2, 0], [1, 0], [0, -1], [0, 1]]
+  attr_reader :moves, :max_distance
+
+  def initialize(color)
+    super(color)
+    @moves = LEFT_RIGHT + FWD_BACK + DIAGONALS
+    @max_distance = 1
+  end
 end
 
 class Queen < Piece
+  attr_reader :moves, :max_distance
 
+  def initialize(color)
+    super(color)
+    @moves = LEFT_RIGHT + FWD_BACK + DIAGONALS
+    @max_distance = 8
+  end
 end
 
 class Rook < Piece
@@ -53,19 +70,31 @@ class Rook < Piece
 
   def initialize(color)
     super(color)
-    @moves = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    @moves = LEFT_RIGHT + FWD_BACK
     @max_distance = 8
   end
 end
 
 class Bishop < Piece
+  attr_reader :moves, :max_distance
 
+  def initialize(color)
+    super(color)
+    @moves = DIAGONALS
+    @max_distance = 8
+  end
 end
 
 class Knight < Piece
+  attr_reader :moves, :max_distance
 
+  def initialize(color)
+    super(color)
+    @moves = KNIGHT
+    @max_distance = 1
+  end
 end
 
 class Pawn < Piece
-
+  # GRRRRRR
 end
