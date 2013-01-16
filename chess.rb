1077e249efa4c905247aab899255b84b588d119c
Adam and Erik <adam.gilg@gmail.com>
2013-01-16 12:33:34 -0800

    Built a skeleton of various chess pieces, created an eye-pleasing print_board, populated the board with starting pieces, and wrote an execute move method that works.

--- chess.rb
+++ chess.rb
@@ -1,4 +1,4 @@
-require 'chess_pieces'
+require "./chess_pieces.rb"
 
 class Chess
 
@@ -17,6 +17,8 @@ class Chess
     "blackPawn" => "\u265F"
   }
 
+  attr_reader :board
+
   def initialize
     @board = []
     build_board
@@ -33,12 +35,62 @@ class Chess
   end
 
   def populate_board
+    @board[0], @board[1] = populate_side("white")
+
+    black_setup = populate_side("black")
+    @board[7], @board[6] = black_setup[0].reverse, black_setup[1]
+    nil
     # create and put pieces in appropriate positions
     #pieces for black & white
   end
 
-  def print_board
-
+  def populate_side(color)
+    first_row = [
+      Rook.new(color),
+      Knight.new(color),
+      Bishop.new(color),
+      King.new(color),
+      Queen.new(color),
+      Bishop.new(color),
+      Knight.new(color),
+      Rook.new(color)
+    ]
+    second_row = []
+    8.times { second_row << Pawn.new(color) }
+    [first_row, second_row]
   end
 
+  def print_board
+    # cycle through the board, outputting " * " for nil and unicode for each piece otherwise
+    print "  "
+    ("A".."H").each { |char| print " #{char} " }
+    puts
+    8.times do |row|
+      print "#{row} "
+      8.times do |col|
+        piece = @board[row][col]
+        if piece.nil?
+          print " * "
+        else
+          print " #{UNICODE_CHARS[piece.name]} "
+        end
+      end
+      puts
+    end
+  end
+
+  def execute_move(from_coord, to_coord)
+    @board[to_coord[0]][to_coord[1]] = @board[from_coord[0]][from_coord[1]]
+    @board[from_coord[0]][from_coord[1]] = nil
+  end
+
+
 end
+
+# SCRIPT
+
+c = Chess.new
+c.populate_board
+c.print_board
+c.execute_move([1, 0], [2, 0])
+c.print_board
--- chess_pieces.rb
+++ chess_pieces.rb
@@ -3,20 +3,42 @@
 
 class Piece
 #ask ned about way to change attrs to attr?
-  attr_accessor :x_coord, :y_coord, :dead, :moved
+  attr_accessor :x_coord, :y_coord, :moved
   attr_reader :color
 
-  alias_method :dead?, :dead
   alias_method :moved?, :moved
 
   def initialize(color)
     @color = color
-    @dead = false
     @moved = false
   end
 
   def name
     @color + self.class.to_s
   end
+
+end
+
+class King < Piece
+
+end
+
+class Queen < Piece
+
+end
+
+class Rook < Piece
+
+end
+
+class Bishop < Piece
+
+end
+
+class Knight < Piece
+
+end
+
+class Pawn < Piece
 
 end
