c195817ab8ff6e732548bc665d16e44596081fbf
App Academy <app-academy@rodin.att.net>
2013-01-16 10:32:37 -0800

    Started building out a blank chess board.

---
+++ chess.rb
@@ -0,0 +1,23 @@
+class Chess
+
+  def initialize
+    @board = []
+    build_board
+  end
+
+  def build_board
+    8.times do
+      tmp = []
+      8.times do
+        tmp << nil
+      end
+      @board << tmp
+    end
+  end
+
+  def populate_board
+    # create and put pieces in appropriate positions
+    #pieces for black & white
+  end
+
+end
